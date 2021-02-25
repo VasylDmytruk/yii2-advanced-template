a6acaa99dba7dc289a053107408a8a5d57fa5a56
Vasyl Dmytruk <snapget@gmail.com>
2021-02-25 23:18:14 +0200

    Added env Yii.php

---
+++ _protected/environments/dev/_protected/Yii.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autocompletion.
+ */
+class Yii extends \yii\BaseYii
+{
+	/**
+	 * @var BaseApplication|WebApplication|ConsoleApplication the application instance
+	 */
+	public static $app;
+}
+
+/**
+ * Class BaseApplication
+ * Used for properties that are identical for both WebApplication and ConsoleApplication
+ *
+ */
+abstract class BaseApplication extends yii\base\Application
+{
+}
+
+/**
+ * Class WebApplication
+ * Include only Web application related components here
+ *
+ * @property \common\components\Formatter $formatter
+ */
+class WebApplication extends yii\web\Application
+{
+}
+
+/**
+ * Class ConsoleApplication
+ * Include only Console application related components here
+ */
+class ConsoleApplication extends yii\console\Application
+{
+}
